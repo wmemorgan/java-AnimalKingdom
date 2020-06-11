6ed9918bcbeec1b6417289ca0efcb31749e37dfb
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-06-12 00:08:56 +0800

    Create abstract class for animals

---
+++ src/animalkingdom/Animal.java
@@ -0,0 +1,20 @@
+package animalkingdom;
+
+abstract class Animal {
+    protected int maxId = 0;
+
+    protected int id;
+    protected String name;
+    protected int year;
+
+    public Animal(String name, int year) {
+        maxId++;
+        id = maxId;
+        this.name = name;
+        this.year = year;
+    }
+
+    String eat() {
+        return "Eat";
+    }
+}

c17b992d5cecf5c5f45d9df4269a5e4ed2bb1d9d
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-08 13:28:08 +0800

    Initialize project

---
+++ src/animalkingdom/Main.java
@@ -0,0 +1,7 @@
+package animalkingdom;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\n*** Welcome to Cafe Morgan's Animal Kingdom ***\n");
+    }
+}
